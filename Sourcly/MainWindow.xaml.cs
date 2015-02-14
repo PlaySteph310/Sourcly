88dfef1df3fd7cbb7601dd3597ff637da0254f70
Stephan H. <stephan_hoehn@gmx.de>
2015-02-14 23:45:51 +0100

    Version 0.0.2
    - new: can use the internet explorer
    - new: can use the default browser

--- Sourcly/MainWindow.xaml.cs
+++ Sourcly/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Sourcly
 {
@@ -25,9 +27,27 @@ namespace Sourcly
         public string filter = "HTML|*.html|TXT|*.txt|CSS|*.css";
         public MainWindow()
         {
+            if (Directory.Exists(Environment.ExpandEnvironmentVariables(ParsePath(@"%localappdata%\\Sourcly\"))))
+            {
+                if (Directory.Exists(Environment.ExpandEnvironmentVariables(ParsePath(@"%localappdata%\\Sourcly\webfiles\"))))
+                {
+
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ParsePath(@"%localappdata%\\Sourcly\")));
+                Directory.CreateDirectory(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ParsePath(@"%localappdata%\\Sourcly\webfiles\")));
+            }
             InitializeComponent();
         }
-
+        public static string ParsePath(string path) //function by dr4yyee
+        {
+            var newPath = new StringBuilder();
+            var folders = path.Split(System.IO.Path.DirectorySeparatorChar);
+            foreach (var folder in folders) newPath.Append((Regex.IsMatch(folder, "%.+%")) ? Environment.GetEnvironmentVariable(Regex.Match(folder, "(?:%)(.+)(?:%)").Groups[1].Value) : folder).Append((folders[folders.Length - 1] == folder) ? string.Empty : new string(System.IO.Path.DirectorySeparatorChar, 1));
+            return newPath.ToString();
+        }
         private async void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             string textbox = text.Text;
@@ -53,22 +73,13 @@ namespace Sourcly
                 text.Text = file;
             }
         }
-        private async void MenuItem_Click2(object sender, RoutedEventArgs e)
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
         {
             MessageBoxResult newfile = MessageBox.Show("Möchten Sie die Datei speichern oder verwerfen?","Bestätigen", MessageBoxButton.YesNoCancel);
             switch(newfile)
             {
                 case MessageBoxResult.Yes:
-                    string textbox = text.Text;
-                    SaveFileDialog savefile = new SaveFileDialog();
-                    savefile.FileName = "unbenannt";
-                    savefile.Filter = filter;
-                    if (savefile.ShowDialog() == true)
-                    {
-                        System.IO.StreamWriter file = new System.IO.StreamWriter(savefile.FileName);
-                        await file.WriteLineAsync(textbox);
-                        file.Close();
-                    }
+                    SaveFiles();
                     break;
                 case MessageBoxResult.No:
                     text.Text = "";
@@ -79,10 +90,91 @@ namespace Sourcly
                     break;
             }
         }
+        public bool SaveFiles()
+        {
+            string textbox = text.Text;
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = "unbenannt";
+            savefile.Filter = filter;
+            if (savefile.ShowDialog() == true)
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(savefile.FileName);
+                file.WriteLine(textbox);
+                file.Close();
+                //lol = true;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
 
         private void datei_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void MenuItem_print(object sender, RoutedEventArgs e)
+        {
+            /*PrintDialog print = new PrintDialog();
+            PrintDocument doc = new PrintDocument();
+            doc.DocumentName = text.Text;
+            print.PrintDocument(doc.DocumentName);
+            if (print.ShowDialog() == true)
+            {
+                //print.PrintDocument(text.Text);
+            }*/
+            MessageBox.Show("Bald verfügbar");
+        }
+        private void PrintDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+        }
+        private void textblock_header_1(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+        }
+
+        private void MenuItem_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_beenden(object sender, RoutedEventArgs e)
+        {
+            if (text.Text == "")
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                switch(MessageBox.Show("Möchten Sie den Text speichern?", "Bestätigen", MessageBoxButton.YesNoCancel))
+                {
+                    case MessageBoxResult.Yes:
+                        if (SaveFiles() == true)
+                        {
+                            Application.Current.Shutdown();
+                        }
+                        break;
+                    case MessageBoxResult.No:
+                        Application.Current.Shutdown();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        break;
+
+                }
+            }
+        }
+        private void start_standartbrowser(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ParsePath(@"%localappdata%\\Sourcly\webfiles\file_1.html")), text.Text);
+            string path = ParsePath(@"%localappdata%\\Sourcly\webfiles\");
+            Process.Start("file://" + path + "file_1.html");
+        }
+        private void MenuItem_internet_explorer(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ParsePath(@"%localappdata%\\Sourcly\webfiles\file_1.html")), text.Text);
+            string path = ParsePath(@"%localappdata%\\Sourcly\webfiles\");
+            Process.Start("iexplore.exe", "file://" + path + "file_1.html");
         }
     }
 }
